7cb20d99d8a7f012ddf0d1608e0d255a590c816f
mapperofallmaps <35754554+mapperofallmaps@users.noreply.github.com>
2018-03-07 17:10:25 +1100

    Variables

---
+++ Variables/variables.py
@@ -0,0 +1,51 @@
+greeting = 'Bruce'
+_myName = 'Dlau'
+Dlau93 = 'Good'
+Dlau_93 = 'Hello'
+Greeting = "There"
+print(Dlau_93 + " " + _myName)
+
+age = 24
+print(age)
+
+a = 12
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print()
+
+for i in range(1, a//b):
+    print(i)
+
+print(a + b / 3 - 4 * 12)
+print((((a + b) / 3) - 4) * 12)
+print()
+
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[0])
+print(parrot[-1])
+print(parrot[0:6])
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[-4:-2])
+print(parrot[0:6:2])
+
+numbers = "1, 2, 3, 4, 5, 6, 7"
+print(numbers[0::3])
+string1 = "he's "
+string2 = "probably "
+print(string1 + string2)
+print("Hello " * 5)
+print("Hello " * (5 + 4))
+print("Hello " * 5 + "4")
+today = "friday"
+print("day" in today)
+print("fri" in today)
+print("thurs" in today)
+print("parrot" in today)
+
