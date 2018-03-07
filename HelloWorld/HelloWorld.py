662d99e9835075e9a6e872c51df59303020970fb
mapperofallmaps <35754554+mapperofallmaps@users.noreply.github.com>
2018-03-07 15:43:18 +1100

    First Python commit

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,18 @@
+# greeting = "Hello"
+# name = input("Please enter your name ")
+# print(greeting + ' ' + name)
+
+# splitString = "this string has been\nsplitover\nseveral\nlines"
+# print(splitString)
+#
+# tabbedString = "1\t2\t3\t4\t5\t"
+# print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting"')
+
+anotherSplitString = """this string has been
+split over
+several lines"""
+print(anotherSplitString)
+
+print('''The pet shop owner said "No, no, 'e's' uh,...he's resting"''')
