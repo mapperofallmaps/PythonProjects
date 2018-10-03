f4b4070235ec5a18b539ffb5906ccac47390587d
dlau <dlau@homeaway.com>
2018-10-03 13:25:09 +1000

    added config file

---
+++ config.py
@@ -0,0 +1,19 @@
+hosts = {
+    'test': 'http://api-test.homeaway.com',
+    'stage': 'http://api-stage.homeaway.com',
+    'prod': 'http://api.homeaway.com',
+}
+
+operations = {
+    'update_affiliate_link': 'updateUnitDistributionWebsiteConfiguration',
+    'delete_site_affiliate': 'removeUnitDistributionWebsiteConfiguration'
+}
+
+system_codes = [
+    '0000',
+    '0001',
+    '0002',
+    '0003',
+    '0004'
+]
+
